f2edc8b17bfcae5a543771d2a4f1038772129abf
TerranceG427 <54366338+TerranceG427@users.noreply.github.com>
2020-12-09 02:54:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,20 +6,23 @@
 <body>
 
 <!---Source code below reference from W3Schools -->
+<!---https://www.w3schools.com/howto/howto_js_temperature_converter.asp -->
 
 <h2>Terrance Whitehead</h2>
 
 <p>Given the current temperature in degrees Kelvin, determine the temperature in degrees Celsius:</p>
 
 <p>
-  <label>Kelvin:<label>
+  <label>degrees Kelvin:<label>
   <input id="inputKelvin" type="number" placeholder="Enter Temperature" oninput="temperatureConverter(this.value)" onchange="temperatureConverter(this.value)">
 </p>
-<p>Celcius: <span id="outputCelcius"></span></p>
+<p>degrees Celcius: <span id="outputCelcius"></span></p>
 
 
 <script>
+
 /*Here below, a function was made to formulate whatever input is entered, a - (negative) output will exploint, in Celsius. -273.15 is a placeholder and a start.*/
+
 function temperatureConverter(valNum) {
   valNum = parseFloat(valNum);
   document.getElementById("outputCelcius").innerHTML=valNum-273.15;
